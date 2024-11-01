37e81ec3991130e17a92c23136c1ced95d4a4549
Netanel Bliacher <nbliacher@gmail.com>
2024-11-01 20:32:03 +0200

    [dev]: Exercis10

---
+++ homeworks/homework1/ex10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int odd_counter = 0;
+    int num;
+    int last_digit;
+    printf("enter a number: ");
+    scanf("%d", &num);
+    num = abs(num);
+    while (num > 1)
+    {
+        last_digit = num % 10;
+        if (0 == last_digit % 2)
+        {
+            odd_counter++;
+        }
+        num = num / 10;
+    }
+     printf("The number of odd digits%d\n", odd_counter);
+    return 0;
+}
